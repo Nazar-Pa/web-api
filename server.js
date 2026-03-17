56bd4a90f93a9f4b8c342a6f0b992ce5cf58fe1a
Nazar-Pam <nezer.pashayev@gmail.com>
2026-03-17 22:12:19 +0400

    change header

--- server.js
+++ server.js
@@ -137,9 +137,23 @@ app.get("/api/turbo", async (req, res) => {
     const url = buildTurboUrl(filters);
 
     const response = await axios.get(url, {
+      // headers: {
+      //   "User-Agent": "Mozilla/5.0",
+      //   "Accept-Language": "az,en;q=0.9"
+      // },
       headers: {
-        "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "az,en;q=0.9"
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+          "(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+
+        "Accept-Language": "az,en-US;q=0.9,en;q=0.8",
+
+        "Connection": "keep-alive",
+
+        "Upgrade-Insecure-Requests": "1"
       },
       timeout: 20000
     });
